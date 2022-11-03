2a11e8be2af46bc7febe59bb9e065cc875fbda84
aviehu <aviehu@gmail.com>
2022-11-03 18:29:33 +0200

    camera and screenshot

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express'
+import bodyParser from "body-parser";
+import cors from 'cors'
 import * as dotenv from 'dotenv'
+import fs from 'fs'
 dotenv.config()
 
 const app = express()
 const port = process.env.port
 
+app.use(cors())
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/image', (req, res) => {
+    const img = req.body.image.split(';base64,').pop();
+    fs.writeFile('image.png', img, {encoding: 'base64'}, function(err) {
+        console.log('File created');
+    });
 })
 
 app.listen(port, () => {
