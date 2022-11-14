3d3518c9004e6285f657049b41219b12bb0c42a7
aviehu <aviehu@gmail.com>
2022-11-14 15:37:51 +0200

    refactor

--- index.js
+++ index.js
@@ -11,24 +11,36 @@ const app = express()
 const port = process.env.port
 
 app.use(cors())
+
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/image', (req, res) => {
-    const img = req.body.image.split(';base64,').pop();
-    fs.writeFile('skeleton-py/pose.png', img, {encoding: 'base64'}, function(err) {
-        console.log('File created');
+function writeFile(img) {
+    return new Promise((resolve) => {
+        fs.writeFile('skeleton-py/pose.png', img, {encoding: 'base64'}, function(err) {
+            resolve()
+        })
+    })
+}
+
+function getSkeleton() {
+    return new Promise((resolve) => {
         const process = spawn('python',["skeleton-py/skeleton.py"])
         process.stdout.on('data',function(data) {
             const ans = JSON.parse(data.toString())
-            console.log(ans)
-            res.json(JSON.stringify(ans))
+            resolve(ans)
         })
-    });
+    })
+}
 
+app.post('/image', async (req, res) => {
+    const img = req.body.image.split(';base64,').pop();
+    await writeFile(img)
+    const ans = await getSkeleton()
+    res.json(JSON.stringify(ans))
 })
 
 app.listen(port, () => {
