c38662c93d92d4fafdbbf1125c95616aa52f5e8b
hilaklein <63915907+hilaklein@users.noreply.github.com>
2023-06-12 22:11:49 +0300

    getuserscore

--- src/controllers/scoreController.js
+++ src/controllers/scoreController.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const userSessionScore = require('../models/scoreModel');
+const Session = require('../models/sessionModel');
 
 exports.addScore = async (req, res) => {
     try {
@@ -23,7 +24,13 @@ exports.getSessionScores = async (req, res) => {
 exports.getUserScores = async (req, res) => {
     try {
         const scores = await userSessionScore.find({user: req.params.user});
-        res.status(StatusCodes.OK).json(scores);
+        const scoresWithSessions = await Promise.all(scores.map(async (score) =>{
+            const session = await Session.findOne({name: score.session})
+            score.session = session
+            return score
+        }))
+        console.log(scoresWithSessions)
+        res.status(StatusCodes.OK).json(scoresWithSessions);
     } catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
     }
